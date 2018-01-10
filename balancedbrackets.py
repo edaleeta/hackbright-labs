9a693fdf1be9816d8686226295e4909e49400854
Edelita Valdez <edaleeta@gmail.com>
2018-01-10 12:51:14 -0800

    Made code more elegant

--- balancedbrackets.py
+++ balancedbrackets.py
@@ -45,14 +45,14 @@ If you receive a string with no brackets, consider it balanced::
    True
 
 """
+BRACKET_PAIRS = {'(': ')', '{': '}', '[': ']', '<': '>',
+                 ')': '(', '}': '{', ']': '[', '>': '<'}
+
 
 def is_anagram(brackets):
     """Tests to see if list is an anagram."""
 
-    reversed_brackets = {'(': ')', '{': '}', '[': ']', '<': '>',
-                         ')': '(', '}': '{', ']': '[', '>': '<'}
-
-    reversed_and_flipped = [reversed_brackets[bracket] for bracket in brackets[::-1]]
+    reversed_and_flipped = [BRACKET_PAIRS[bracket] for bracket in brackets[::-1]]
 
     if reversed_and_flipped == brackets:
         return True
@@ -66,15 +66,13 @@ def has_balanced_brackets(phrase):
     string contains balanced (), {}, [], and/or <>.
     """
 
-    brackets = ['(', ')', '{', '}', '[', ']', '<', '>']
-
     brackets_in_phrase = []
 
     """march through phrase, look for bracket.
     if we find a bracket, let's test to see if it's an anagram"""
 
     for letter in phrase:
-        if letter in brackets:
+        if letter in BRACKET_PAIRS:
             brackets_in_phrase.append(letter)
 
     if len(brackets_in_phrase) % 2 == 1:
