44dddfb094d5dae23231ab82e0f0bfed43c9c9b5
Edelita Valdez <edaleeta@gmail.com>
2018-01-26 13:02:31 -0800

    Finishes Order Melons and error message styling.

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -7,7 +7,9 @@ function showFortune(evt) {
 
     // TODO: get the fortune and show it in the #fortune-text div
 
-    $.get('/fortune', (result)=>{$('#fortune-text').html(result)});
+    $.get('/fortune', (result)=> {
+        $('#fortune-text').html(result);
+    });
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -26,6 +28,10 @@ function showWeather(evt) {
 
 
     // TODO: request weather with that URL and show the forecast in #weather-info
+
+    $.get(url, formData, (weather_info) => {
+        $('#weather-info').html(weather_info['forecast']);
+    });
 }
 
 $("#weather-form").on('submit', showWeather);
@@ -39,8 +45,29 @@ function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    let url = '/order-melons.json';
+    let formData = {'melon_type': $('#melon-type-field').val(), 
+                    'qty': $('#qty-field').val()};
+
+    $.post(url, formData, showOrderStatus);
+
 }
+
+// TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+
+function showOrderStatus(result) {
+
+    $('#order-status').html(result['msg']);
+
+    if (result['code'] === 'ERROR') {
+        $('#order-status').addClass('order-error');
+    } else {
+        $('#order-status').removeClass('order-error');
+    }
+}
+
 
 $("#order-form").on('submit', orderMelons);
 
